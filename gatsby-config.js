c9a2cb20704a11cdac4cb4aa69e4174513e5dd56
h15200 <45977354+h15200@users.noreply.github.com>
2021-01-23 16:14:29 -0800

    swap out gatsby-prefetch-plugin for gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: 'Steve Kenyon',
-    siteUrl: 'https://www.stevekenyon.com',
-    description: 'Steve Kenyon - Musician'
+    title: "Steve Kenyon",
+    siteUrl: "https://www.stevekenyon.com",
+    description: "Steve Kenyon - Musician",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/images`,
-        name: 'images'
-      }
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-html-attributes',
-      options: {
-        lang: 'en'
-      }
-    },
-    'gatsby-plugin-favicon',
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: 'Poppins',
-            variants: ['400', '500', '600', '700'],
-          },
-        ],
+        name: "images",
       },
-    }
-  ]
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en",
+      },
+    },
+    "gatsby-plugin-favicon",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Poppins\:400,500,600,700`],
+        display: "swap",
+      },
+    },
+  ],
 }
